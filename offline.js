82fde3edc0bfd2ba3be172a1d28fccd6cf776a8b
nick campbell <nick campbell>
2017-05-29 17:40:58 -0400

    Fixed a glitch

---
+++ offline.js
@@ -0,0 +1,22 @@
+{
+	"version": 1496094042,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/background.png",
+		"images/platform-sheet0.png",
+		"images/particlered.png",
+		"images/player-sheet0.png",
+		"images/player-sheet1.png",
+		"images/player-sheet2.png",
+		"images/levelexit-sheet0.png",
+		"images/colorpreview-sheet0.png",
+		"images/particleorange.png",
+		"images/particleyellow.png",
+		"images/particlegreen.png",
+		"images/particleblue.png",
+		"images/particlepurple.png"
+	]
+}
